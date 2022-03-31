fd5c925528bb1da9a152eb0d44d7716840780066
ChenXiao Liu <810196813@qq.com>
2022-03-31 17:35:50 -0500

    complete popover

--- modules/frontend/lobby.js
+++ modules/frontend/lobby.js
@@ -1,11 +1,43 @@
 const user = {};
 
+function closePopover() {
+  document.getElementById('popover').remove();
+}
+
+function createRoomButtonDidClick(event) {
+  event.preventDefault();
+  const popover = document.createElement('div');
+  popover.id = 'popover';
+  const closeBtn = document.createElement('i');
+  closeBtn.className = 'iconfont icon-close';
+  closeBtn.addEventListener('click', closePopover);
+  const roomInput = document.createElement('input');
+  roomInput.type = 'text';
+  roomInput.id = 'roomName';
+  roomInput.required = true;
+  const label = document.createElement('label');
+  label.for = 'roomName';
+  label.textContent = 'Room Name';
+  const submitButton = document.createElement('button');
+  submitButton.textContent = 'Create Room';
+  const form = document.createElement('form');
+  form.className = 'form-wrapper';
+  form.addEventListener('click', (e) => { e.stopPropagation(); });
+  form.append(closeBtn, roomInput, label, submitButton);
+  // const content = document.createElement('div');
+  // content.className = 'popover-content';
+  popover.addEventListener('click', closePopover);
+  popover.appendChild(form);
+  document.body.appendChild(popover);
+}
+
 export default function loadLobby(id, nickname) {
   user.id = id;
   user.nickname = nickname;
   const sidebar = document.createElement('aside');
   const createRoomButton = document.createElement('button');
   createRoomButton.className = 'iconfont icon-plus';
+  createRoomButton.addEventListener('click', createRoomButtonDidClick);
   sidebar.append(createRoomButton);
   const nav = document.createElement('nav');
   const main = document.createElement('main');
